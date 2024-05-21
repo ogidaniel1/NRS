7d89ae761e8108d93935e7b69d58bd9e20cbd9ec
macdanniels <hoghidan1@gmail.com>
2024-05-21 20:37:05 +0100

    updating app.py

--- NR/NRApp/app.py
+++ NR/NRApp/app.py
@@ -1,16 +1,23 @@
-from flask import Flask, render_template, request
+
+from flask import Flask, render_template,flash, session, request
+from datetime import timedelta
+from flask_sqlalchemy  import SQLAlchemy
+import numpy as np
+
 import pickle, sqlite3
 
 
 app = Flask(__name__)
+app.secret_key = "hello"
 
 #declaring load model from ml codebase...pickle file
-cv = pickle.load(open("../notebook/catboost_model.pkl", 'rb'))
+cv = pickle.load(open("../notebook/xgboost.joblib", 'rb'))
 
 @app.route("/")
 #homepage route...........
 def home():
     #request from the user page............
+    flash("hello")
     return render_template("index.html")
     
 
@@ -20,14 +27,17 @@ def predict():
     if request.method == 'POST':
         # print()
     #all the list of features collected on the form as alist
-    
-        feature_list = [
-            'business_project','proposed_facility_amount','value_chain_category','borrowing_relationship',
-            'fresh_loan_request','request_submitted_to_bank','feasibility_study_available'
-            ]
         
-        features = [request.form[field] for field in feature_list] #getting values from the html form
-        prediction = cv.predict(features)
+        feature_list = [{
+            'BUSINESS_PROJECT':1, 'VALUE_CHAIN_CATEGORY':2,
+                          'BORROWING_RELATIONSHIP':1, 'FRESH_LOAN_REQUEST':1,
+                          'REQUEST_SUBMITTED_TO_BANK':1, 'FEASIBILITY_STUDY_AVAILABLE':0, 'PROPOSED_FACILITY_AMOUNT':2000000000
+            }]
+        
+        features = [request.form[field] for field in feature_list] #getting input values from the html form
+        feature= np.array[[features]]
+        print(feature)
+        prediction = cv.predict(feature)
         prediction = 1 if prediction == 1 else -1
          
         return render_template("index.html", prediction = prediction) 
